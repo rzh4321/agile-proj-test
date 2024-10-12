66d65d6be49c58d3ea40ff93c7f52672bb2a62a9
rzh4321 <rzh4321@gmail.com>
2024-10-11 21:52:40 -0400

    added toggleFilterURL

--- frontend/src/components/Filters.tsx
+++ frontend/src/components/Filters.tsx
@@ -1,6 +1,28 @@
 import PriceRangeFilters from "./PriceRangeFilters";
+import { useSearchParams } from "react-router-dom";
 
 export default function Filters({currentFilter} : {currentFilter: string}) {
+    const [searchParams, setSearchParams] = useSearchParams();
 
-    if (currentFilter === 'Price Range') return <PriceRangeFilters />
+    // toggle filter from URL
+    const toggleFilterURL = (filterType: string, value: string) => {
+        const currentParams = new URLSearchParams(searchParams);
+        const currentValues = currentParams.getAll(filterType);
+        
+        if (currentValues.includes(value)) {
+          // remove  value if  exists
+          currentParams.delete(filterType);
+          currentValues.filter(v => v !== value).forEach(v => {
+            // add search param values back after filtering
+            currentParams.append(filterType, v);
+          });
+        } else {
+          // add  value if  doesn't exist
+          currentParams.append(filterType, value);
+        }
+        
+        setSearchParams(currentParams);
+      };
+
+    if (currentFilter === 'Price Range') return <PriceRangeFilters toggleFilterURL={toggleFilterURL} />
 }
--- frontend/src/components/PriceRangeFilters.tsx
+++ frontend/src/components/PriceRangeFilters.tsx
@@ -9,16 +9,21 @@ const priceRangeToDollarIcons: Record<PriceRange, number> = {
     Luxury: 5,
   };
 
-export default function PriceRangeFilters() {
+type Props = {
+    toggleFilterURL: (filterType: string, value: string) => void;
+}
+
+export default function PriceRangeFilters({toggleFilterURL} : Props) {
     const priceRanges = ['Budget', 'Mid-Range', 'Premium', 'Luxury'];
     const {toggleFilter, filterIsApplied} = useMyStores();
 
     const handleFilterClick = (priceRange: string) => {
         toggleFilter('priceRange' as keyof Filters, priceRange);
+        toggleFilterURL('priceRange', priceRange);
     }
 
     const priceRangeButtons = priceRanges.map(priceRange => (
-        <Button key={priceRange} onClick={() => handleFilterClick(priceRange)} className={`text-2xl ${filterIsApplied('priceRange' as keyof Filters, priceRange) ? 'bg-green-500 border-green-600 text-white' : 'bg-green-300 text-green-800 border-green-400'}`}>{priceRange} (
+        <Button key={priceRange} onClick={() => handleFilterClick(priceRange)} className={`text-2xl active:bg-green-200 active:text-black active:border:bg-green-300  ${filterIsApplied('priceRange' as keyof Filters, priceRange) ? 'bg-green-600 border-green-700 hover:bg-green-600 text-white' : 'bg-green-300 hover:bg-green-400 text-green-800 border-green-400'}`}>{priceRange} (
             {"$".repeat(
               priceRangeToDollarIcons[priceRange as PriceRange],
             )}
